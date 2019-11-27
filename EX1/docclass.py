b07ab2db035f15e3902547f2044c68652221b82f
kassama fally <fally.kassama@outlook.com>
2019-11-27 10:05:54 +0100

    construct and commentarie

---
+++ EX1/docclass.py
@@ -0,0 +1,20 @@
+#create class for def town and number town.
+class City :
+#Construct.
+    def __init__(self, ville, departement):
+        self.ville = ville
+        self.departement = departement
+#Method for see where we are.
+    def showlocation(self):
+        print("la ville est {}".format(self.ville),"et le departement est {}".format(self.departement))
+#Method for change our location .
+    def changelocation(self,ville,departement):
+        self.ville = ville
+        self.departement = departement
+
+
+villeactuelle = City("Paris",75)
+villeactuelle.showlocation()
+
+villeactuelle.changelocation("Marseille",13)
+villeactuelle.showlocation()
